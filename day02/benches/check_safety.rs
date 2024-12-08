e8c241b813f36d92486abc5baa19fc9c62de5f2a
Andreas Lösel <andreas.loesel@outlook.com>
2024-12-08 16:27:12 +0100

    Fix and improve benches of day2

--- day02/benches/check_safety.rs
+++ day02/benches/check_safety.rs
@@ -1,14 +1,20 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use day02::check_safety_dampened as check_safety;
+use day02::check_safety;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
 fn benchmark_check_safety(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(42);
 
-    let mut arr1: Vec<i32> = (0..100).map(|_| rng.gen_range(0..100)).collect();
+    let mut vec_of_data: Vec<Vec<i32>> = (0..1000)
+        .map(|_| (0..10).map(|_| rng.gen_range(0..100)).collect())
+        .collect();
 
-    c.bench_function("check_safety (random values)", |b| {
-        b.iter(|| check_safety(black_box(&mut arr1)))
+    c.bench_function("check_safety (1000x10 random values)", |b| {
+        b.iter(|| {
+            for i in 0..1000 {
+                check_safety(black_box(&mut vec_of_data[i]));
+            }
+        })
     });
 }
 
--- day02/benches/check_safety_dampened.rs
+++ day02/benches/check_safety_dampened.rs
@@ -5,10 +5,16 @@ use rand::{rngs::StdRng, Rng, SeedableRng};
 fn benchmark_check_safety_dampened(c: &mut Criterion) {
     let mut rng = StdRng::seed_from_u64(42);
 
-    let mut arr1: Vec<i32> = (0..100).map(|_| rng.gen_range(0..100)).collect();
+    let mut vec_of_data: Vec<Vec<i32>> = (0..1000)
+        .map(|_| (0..10).map(|_| rng.gen_range(0..100)).collect())
+        .collect();
 
-    c.bench_function("check_safety_dampened (random values)", |b| {
-        b.iter(|| check_safety_dampened(black_box(&mut arr1)))
+    c.bench_function("check_safety_dampened (1000x10 random values)", |b| {
+        b.iter(|| {
+            for i in 0..1000 {
+                check_safety_dampened(black_box(&mut vec_of_data[i]));
+            }
+        })
     });
 }
 
